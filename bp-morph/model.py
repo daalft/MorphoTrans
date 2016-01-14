0837386d64f194ad2d44b4aaa1a7edf8d8b51874
Ryan Cotterell <ryan.cotterell@gmail.com>
2016-01-14 16:26:07 +0100

    almost done training

--- bp-morph/model.py
+++ bp-morph/model.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy import eye, zeros, exp, log, dot, outer
+from scipy.spatial.distance import hamming
 import itertools as it
+from scipy.optimize import fmin_l_bfgs_b as lbfgs
 
 class TagPair(object):
     """
@@ -23,9 +25,10 @@ class RRBM(object):
 
     """
 
-    def __init__(self, N, train):
+    def __init__(self, N, train, C = 0.01):
         self.N = N
         self.train = train
+        self.C = C  # regularization coefficient
         self.W = np.random.rand(N, N)
         #self.W = eye(N)
         self.b = np.random.random(N)
@@ -34,6 +37,7 @@ class RRBM(object):
         
     def Z(self, W, b, x):
         """ Computes the log partition function Z(x) """
+        
         Z = 1.0
         x_W  = dot(x, W)
         for i in xrange(self.N):
@@ -55,36 +59,78 @@ class RRBM(object):
     
     def f(self, W, b):
         """ Log-likelihood of the training data """
+        
         ll = 0
         for x, y in self.train:
             score = dot(dot(x, W), y) + dot(b, y)
             Z = self.Z(W, b, x)
-            ll += score - log(Z)
+            ll -= (score - log(Z))
             
         return ll
 
     
     def g(self, W, b):
         """ Computes the gradient of the log-likelihood """
+        
         W_g, b_g = zeros((self.N, self.N)), zeros((self.N))
         for x, y in self.train:
-            #W_ += y
             tmp = exp(dot(x, W) + b)
             marg = tmp / (1 + tmp)
-            W_g += outer(x, y - marg)
-            b_g += y - marg
+            W_g -= outer(x, y - marg)
+            b_g -= (y - marg)
             
         return W_g, b_g
     
 
+    def predict(self, x):
+        """ predicts a y given an x using the parameters of the model """
+        
+        tmp = exp(dot(x, self.W) + self.b)
+        marg = tmp / (1 + tmp)
+        return np.round(marg)
+
+
+    def eval(self, heldout):
+        """ evaluate under two metrics: 1-best error rate and hamming """
+        
+        err, ham  = 0, 0
+        N = float(len(heldout))
+        for x, y in heldout:
+            y_pred = self.predict(x)
+            err += 0.0 if y == y_pred else 0
+            ham += hamming(y, y_pred)
+
+        return err / N, ham / N
+        
+
+    def learn(self):
+        """ trains the model using batch L-BFGS """
+
+        def ff(vec):
+            W, b = self.vec2theta(vec)
+            return self.f(W, b) + self.C / 2.0 * np.dot(vec, vec)
+
+        def gg(vec):
+            W, b = self.vec2theta(vec)
+            gW, gb = self.g(W, b)
+            return self.theta2vec(gW, gb) + self.C * vec
+        
+        v = self.theta2vec(self.W, self.b)
+        #lbfgs(
+        self.W, self.b = self.vec2theta(v)
             
     def theta2vec(self, W, b):
         """ converts the parameters W and b to one long vector """
+        
         return np.concatenate((np.asarray(W).reshape(-1), b))
 
+    
     def vec2theta(self, vec):
         """ converts the vector to the parameter W and b """
+        
         lst = np.split(vec, [self.N**2, len(vec)])
         W = lst[0].reshape(self.N, self.N)
         b = lst[1]
         return W, b
+
+    
--- bp-morph/test_rrbm.py
+++ bp-morph/test_rrbm.py
@@ -1,6 +1,7 @@
 from model import RRBM
 import numpy as np
 from numpy import zeros
+from scipy.optimize import fmin_l_bfgs_b as lbfgs
 
 def fd(model, EPS=0.01):
     """ finite difference check f """
@@ -31,14 +32,20 @@ def fd(model, EPS=0.01):
     return gW_fd, gb_fd
 
 N = 5
-T = 10
+T_train, T_test = 10, 10
 
-train = []
-for i in xrange(T):
+train, test = [], []
+for i in xrange(T_train):
     x = np.random.choice([0, 1], size=(N,), p=[1./3, 2./3])
     y = np.random.choice([0, 1], size=(N,), p=[1./3, 2./3])
     train.append((x, y))
 
+for i in xrange(T_test):
+    x = np.random.choice([0, 1], size=(N,), p=[1./3, 2./3])
+    y = np.random.choice([0, 1], size=(N,), p=[1./3, 2./3])
+    test.append((x, y))
+
+    
 model = RRBM(N, train)
 # check partition function
 assert np.allclose(model.Z(model.W, model.b, train[0][0]), model.enum_Z(model.W, model.b, train[0][0]), atol = 0.01)
@@ -54,3 +61,5 @@ assert np.allclose(gb, gb_fd, atol=0.01)
 W2, b2 = model.vec2theta(model.theta2vec(model.W, model.b))
 assert (W2 == model.W).all()
 assert (b2 == model.b).all()
+
+# check optimization
